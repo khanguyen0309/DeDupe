4ca7fe486830527df01f58a4259ad4f429a2dcff
Kha Nguyen <166071856+khanguyen0309@users.noreply.github.com>
2026-04-03 17:04:24 -0400

    Create Struct with index and hash. Each chunk will has its own information for later multithreaded speedup

--- Project 2/dedupe.c
+++ Project 2/dedupe.c
@@ -5,11 +5,16 @@
 
 #include "hash_functions.h"
 
+typedef struct {
+    int index;
+    unsigned char *hash;
+} ChunkHash;
+
 int compare_hashes(unsigned char *a, unsigned char *b, int n) {
-	for(int i=0; i < n; i++)
-		if(a[i] != b[i])
-			return 0;
-	return 1;
+    for (int i = 0; i < n; i++)
+        if (a[i] != b[i])
+            return 0;
+    return 1;
 }
 
 // Function name: dedupe
@@ -18,25 +23,33 @@ int compare_hashes(unsigned char *a, unsigned char *b, int n) {
 void dedupe(char *filename, int chunk_size, char *output) {
 	FILE *fp;
 	char *buffer = (char *) malloc(chunk_size*sizeof(char));
-	unsigned char **hashes = NULL;
 	int hash_size = size_sha512(), n_hashes = 0;
 
+	ChunkHash *chunks = NULL;
+	int n_chunks = 0;
+	
 	// load chunks of the input file and hash them
 	fp = fopen(filename, "r");
 	assert(fp != NULL);
+
+	
 	while(fread(buffer, sizeof(char), chunk_size, fp) == chunk_size) {
-		hashes = (unsigned char **) realloc(hashes, (n_hashes+1)*sizeof(unsigned char *));
-		hashes[n_hashes] = calculate_sha512((unsigned char *)buffer, chunk_size);
-		n_hashes++;
+        chunks = (ChunkHash *) realloc(chunks, (n_chunks + 1) * sizeof(ChunkHash));
+        assert(chunks != NULL);
+
+        chunks[n_chunks].index = n_chunks;
+        chunks[n_chunks].hash = calculate_sha512((unsigned char *)buffer, chunk_size);
+
+        n_chunks++;
 	}
 	fclose(fp);
 
-	int mask[n_hashes];
-	for(int i=0; i < n_hashes; i++)
+	int mask[n_chunks];
+	for(int i=0; i < n_chunks; i++)
 		mask[i] = 0;
-	for(int i=0; i < n_hashes; i++)
-		for(int j=i+1; j < n_hashes; j++)
-			if(compare_hashes(hashes[i], hashes[j], hash_size)) {	
+	for(int i=0; i < n_chunks; i++)
+		for(int j=i+1; j < n_chunks; j++)
+			if(compare_hashes(chunks[i].hash, chunks[j].hash, hash_size)) {	
 				mask[j] = 1;
 				break;
 			}
@@ -44,15 +57,15 @@ void dedupe(char *filename, int chunk_size, char *output) {
 	// print results
 	fp = fopen(output, "w");
 	assert(fp != NULL);
-	for(int i=0; i < n_hashes; i++)
+	for(int i=0; i < n_chunks; i++)
 		fprintf(fp, "%d", mask[i]);
 	fprintf(fp, "\n");
 	fclose(fp);
 
 	// release stuff
 	free(buffer);
-	for(int i=0; i < n_hashes; i++)
-		free(hashes[i]);
-	free(hashes);
+	for(int i=0; i < n_chunks; i++)
+		free(chunks[i].hash);
+	free(chunks);
 }
 
